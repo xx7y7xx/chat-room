4a7b148a6d2f3c06428afe3c7a0a85eecbafc0b9
Devin Chen <d3vin.chen@gmail.com>
2017-09-14 13:06:20 +0800

    show title

--- src/App.js
+++ src/App.js
@@ -82,8 +82,8 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro" style={{ display: 'none' }}>
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p className="App-intro">
+          天朝拆迁队
         </p>
         <ul id="messages">
           {this.state.messages.map(
